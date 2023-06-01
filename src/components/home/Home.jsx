fba4ec661bb00da56164349c2336ec2410f76cea
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-06-02 04:07:42 +0500

    Add check user

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,9 +4,20 @@ import { useContext } from "react";
 import { Context } from "../../context/Context";
 import Chat from "../chat/Chat";
 import Login from "../login/Login";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
 
 const Home = () => {
   const { state, dispatch } = useContext(Context);
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      const { uid, accessToken, displayName, email, photoURL } = user;
+
+      const payload = { uid, accessToken, displayName, email, photoURL };
+      dispatch({ type: "LOGIN", payload });
+    }
+  });
 
   return (
     <>
