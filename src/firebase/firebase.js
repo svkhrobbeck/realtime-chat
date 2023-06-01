4b017f8062f66b76660e5fdb15716a97428f4790
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-06-02 04:07:54 +0500

    Add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjIgYo9WX2SRE7-jGwbiYe6BcEpnFPNuI",
+  authDomain: "realtime-chat-ca371.firebaseapp.com",
+  projectId: "realtime-chat-ca371",
+  storageBucket: "realtime-chat-ca371.appspot.com",
+  messagingSenderId: "829056449874",
+  appId: "1:829056449874:web:ff750ef9ea4dfbe9f80e8b",
+  measurementId: "G-FLEEQZKY6G",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth(firebaseApp);
