71ce850b99d4d18a5ee4217fa14d139504cf60d5
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-06-02 03:46:47 +0500

    Add components to index

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Chat } from "./chat/Chat";
+export { default as Header } from "./header/Header";
+export { default as Home } from "./home/Home";
+export { default as Login } from "./login/Login";
