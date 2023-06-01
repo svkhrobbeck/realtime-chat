8f48ac062b3b8ebb3bd5bb20deca798de818b662
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-06-02 04:07:08 +0500

    Add login functional

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import "./Login.scss";
 import { Context } from "../../context/Context";
+import { auth, provider } from "../../firebase/firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
   const { dispatch } = useContext(Context);
+
   const handleLogin = () => {
-    dispatch({ type: "LOGIN" });
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const { uid, accessToken, photoURL, email, displayName } = result.user;
+
+        localStorage.setItem("token", accessToken);
+        localStorage.setItem("uid", uid);
+
+        const payload = { uid, accessToken, photoURL, email, displayName };
+        dispatch({ type: "LOGIN", payload });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   return (
