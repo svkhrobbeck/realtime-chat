bd460dc364f3496aa778f1b3958dad45ae58af6a
Suhrobbek Soatov <svkhrobbeck@gmail.com>
2023-06-02 07:29:44 +0500

    remove unnecessary react-router-dom lines and fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,12 @@ import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import ContextProvider from "./context/Context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
